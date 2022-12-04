68d59d98d37d0abd309e619438f885b1ac098624
epicfloe7 <114299368+epicfloe7@users.noreply.github.com>
2022-12-04 15:28:12 +0300

    Update main.py
    no line edit, no third timer working right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit
-from PyQt5.QtCore import Qt
-import time
+from PyQt5.QtCore import Qt, QTime, QTimer
+from PyQt5.QtGui import QFont
 
 app = QApplication([])
 window = QWidget()
@@ -9,7 +9,7 @@ window2 = QWidget()
 window2.resize(900,600)
 window3 = QWidget()
 window3.resize(900,600)
-IS_1_TIMER = False
+
 
 def change_1_to_2():
     window2.setLayout(layout2)
@@ -18,14 +18,52 @@ def change_2_to_3():
     window3.setLayout(screen3)
     window3.show()
 def timer1_start():
-    global start
-    start = time.time()
-    current_time = time.time() - start
-    while current_time < 30:
-        timer_and_counter.setText(f"00:00:{round(current_time)}")
-        print(round(current_time))
-        current_time = time.time() - start
-
+    global times1, timer
+    times1 = QTime(0,0,30)
+    timer = QTimer()
+    timer.timeout.connect(timer1_event)
+    timer.start(1000)
+    times1.toString('hh:mm:ss')
+def timer1_event():
+    global times1, timer
+    times1 = times1.addSecs(-1)
+    times_text = times1.toString('hh:mm:ss')
+    if times_text == '00:00:00':
+        timer.stop()
+    timer_and_counter.setText(times_text)
+def timer2_start():
+    global times1, timer
+    times1 = QTime(0,0,0)
+    timer = QTimer()
+    timer.timeout.connect(timer2_event)
+    timer.start(1500)
+    times1.toString('s')
+def timer2_event():
+    global times1, timer
+    times1 = times1.addSecs(1)
+    times_text = times1.toString('s')
+    if times_text == '30':
+        timer.stop()
+    timer_and_counter.setText(times_text)
+def timer3_start():
+    global times1, timer
+    times1 = QTime(0,1,0)
+    timer = QTimer()
+    timer.timeout.connect(timer3_event)
+    timer.start(1000)
+    times1.toString('hh:mm:ss')
+def timer3_event():
+    global times1, timer
+    times1 = times1.addSecs(-1)
+    timers_text = times1.toString('hh:mm:ss')
+    if int(times1.toString('hh:mm:ss')[6:8]) >= 45:
+        timer_and_counter.setStyleSheet('color: rgb(0,255,0)')
+    if int(times1.toString('hh:mm:ss')[6:8]) <= 15:
+        timer_and_counter.setStyleSheet('color: rgb(0,255,0)')
+    else:
+        timer_and_counter.setStyleSheet('color: rgb(0,0,0)')
+    print(int(timers_text[6:8]))
+    timer_and_counter.setText(timers_text)
 label_introduction = QLabel('Добро пожаловать в программу по определению состояния здоровья!')
 label_rules = QLabel('Данное приложение позволит вам с помощью теста Руфье провести перечную диагностику вашего здоровья.\nПроба Руфье представляет собой нагрузочный комплекс, предназначенный для оценки работоспособности сердца при физической нагрузке.\nУ испытуемого, находящегося в положении лежа на спине в течение 5 мин\nУ испытуемого, находящегося в положении лежа на спине в течение 5 мин\nУ испытуемого, находящегося в положении лежа на спине в течение 5 мин определяют частоту пульса за 15 секунд; затем в течение 45 секунд испытуемый выполняет 30 приседаний.\nПосле окончания нагрузки испытуемый ложится, и у него вновь подсчитывается число пульсаций за первый 15 секунд\nа потом - за последние 15 секунд первой минуты периода восстановления\nВажно! Если в процессе проведения испытания вы почувствуете себя плохо (появится головокружение, шум в ушах, сильная одышка и др.), то\nтест необходимо прервать и обратиться к врачу ')
 button_start = QPushButton('Начать')
@@ -53,7 +91,8 @@ final_button = QPushButton('Начать финальный тест')
 final_text_enter = QLineEdit()
 final1_text_enter = QLineEdit()
 
-timer_and_counter = QLabel("00:00:00")
+timer_and_counter = QLabel('00:00:00')
+timer_and_counter.setFont(QFont("Times", 36, QFont.Bold))
 line_timer.addWidget(timer_and_counter, alignment=Qt.AlignRight)
 button_end = QPushButton('Завершить тест')
 
@@ -83,7 +122,8 @@ window.setLayout(layout1)
 button_start.pressed.connect(change_1_to_2)
 button_end.pressed.connect(change_2_to_3)
 timer1_start_button.pressed.connect(timer1_start)
-
+counter_start_button.pressed.connect(timer2_start)
+final_button.pressed.connect(timer3_start)
 
 
 screen3 = QVBoxLayout()
